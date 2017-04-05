5275f0398f6cf3ed80045fb1f41ee540b587faf7
sagar.koshti <sagar.koshti@LTAH051.sailfin.com>
2017-04-05 17:19:48 +0530

    1. changed index.html
    Sagar Koshti 16:55 05-April-2017

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        Hello 1123
+        Hello Sagar
         <?php 
         // put your code here
         ?>
